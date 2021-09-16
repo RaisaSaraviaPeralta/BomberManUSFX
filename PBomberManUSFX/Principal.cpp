cfa6b06106b6982732483cab091c5184a023b95d
Raisa Isabel Saravia Peralta <raisa_99_11@hotmail.com>
2021-09-16 16:59:12 -0300

    cambios

--- PBomberManUSFX/Principal.cpp
+++ PBomberManUSFX/Principal.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include "Detonador.h"
+
 using namespace std;
 
 int main() {
-	cout << "Raisa Isabel Saravia Peralta" << endl;
-	cout << "35-4555" << endl;
+
+	cout << "Raisa Saravia Peralta" << endl;
+	cout << "CU: 35-4555" << endl;
+
+	Detonador d1;
+
+	d1.setHoras(5);
+	d1.setMinutos(20);
+	d1.setSegundos(10);
+
+	while (d1.getEjecutado() != true) {
+		d1.ReducirTiempo();
+	}
+
+	d1.Explotar();
+
+
 	return 0;
 }
