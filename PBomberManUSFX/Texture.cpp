047c62bb781a457c45f6a679d820947ce39cef18
Raisa Isabel Saravia Peralta <raisa_99_11@hotmail.com>
2021-11-22 22:34:32 -0400

    cmbios texture

--- PBomberManUSFX/Texture.cpp
+++ PBomberManUSFX/Texture.cpp
@@ -1,1 +1,22 @@
 #include "Texture.h"
+
+Texture::Texture()
+{
+	texture = nullptr;
+	ancho = 0;
+	alto = 0;
+}
+
+Texture::~Texture()
+{
+	free();
+}
+void Texture::free() {
+	if (texture != nullptr) {
+		SDL_DestroyTexture(texture);
+		texture = nullptr;
+
+		ancho = 0;
+		alto = 0;
+	}
+}
--- PBomberManUSFX/Texture.h
+++ PBomberManUSFX/Texture.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+
+
 class Texture
 {
+private:
+	SDL_Texture* texture;
+	int ancho;
+	int alto;
+public:
+	Texture();
+	~Texture();
+	void free();
 };
 
